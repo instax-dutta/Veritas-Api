12e65fe444a6fe8b65d219bf0026bfaafeb0c019
instax-dutta <bffsproductionhouse456@gmail.com>
2024-03-31 00:16:32 +0530

    completely refactoring code

--- browser-extension/background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.prediction !== undefined) {
-    const iconPath = message.prediction === 1 
-      ? "red16.png"  
-      : "green16.png";  
-    chrome.action.setIcon({ path: iconPath });
-  }
-});
--- browser-extension/popup.js
+++
@@ -1,5 +0,0 @@
-chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    const url = tabs[0].url;
-    document.getElementById("message").textContent = `Checking ${url}...`;
-  });
-  
