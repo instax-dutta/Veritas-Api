df70b044001991bd7d2ec6e48aa42f4ed67bcfb4
instax-dutta <bffsproductionhouse456@gmail.com>
2024-03-31 21:17:10 +0530

    gend maraoooo

--- popup.js
+++
@@ -1,9 +0,0 @@
-chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, { action: "getCurrentStatus" }, (response) => {
-        const statusEl = document.getElementById('status');
-        if (response) {
-            statusEl.textContent = response.status;
-            statusEl.className = response.prediction; 
-        }
-    });
-});
